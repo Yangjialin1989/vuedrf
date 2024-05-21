e739b2ca2912053a07cd209e8f80b877ed23780e
Yangjialin <729850713@qq.com>
2024-05-21 19:37:38 +0800

    v4

--- src/view/homeView.vue
+++ src/view/homeView.vue
@@ -67,7 +67,7 @@
                 </el-col>
               </el-row>
             </el-form>
-            <el-table :data="data1" border style="width: 100%;" flexible>
+            <el-table ref="multipleTableRef" @selection-change="handleSelectionChange"  :data="data1" border style="width: 100%;" flexible>
               <el-table-column type="selection" label="多选" width="40" />
               <el-table-column type="index" label="序号" width="60" />
               <el-table-column  prop="sno" label="学号" width="80" />
@@ -81,14 +81,14 @@
               <el-table-column label="操作" width="140" fixed="right" v-slot="scope">
                   <el-button @click="viewStudent(scope.row)" type="success" :icon="MoreFilled" circle size="small"/>
                   <el-button @click="editStudent(scope.row)" type="primary" :icon="Message" circle size="small"/>
-                  <el-button type="danger" :icon="Delete" circle size="small"/>
+                  <el-button @click="deleteStudent(scope.row)" type="danger" :icon="Delete" circle size="small"/>
               </el-table-column>
             </el-table>
 
 
             <el-row>
               <el-col :span="8">
-                <el-button type="primary"><el-icon><Search /></el-icon>批量删除</el-button>
+                <el-button @click="studentDeletes()" type="danger" :icon="Delete">批量删除</el-button>
               </el-col>
               <el-col :span="16">
                 <div class="demo-pagination-block">
@@ -111,6 +111,18 @@
 
             <el-dialog :title="dialogTitle" v-model="dialogVisible" width="50%" :close-on-click-modal="false" @close="closeDialogForm()">
               <el-form ref="studentFormRef" :model="studentForm" :rules="rules" :inline="true" size="small" label-width="110px" label-position="right" style="margin-left: 20px;">
+                <el-upload
+                    class="avatar-uploader"
+                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                    :show-file-list="false"
+                    :on-success="handleSuccess"
+                    :before-upload="beforeAvatarUpload"
+                    :on-error="handleError"
+                >
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                  <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+                </el-upload>
+
                 <el-form-item label="学号:" prop="sno">
                   <el-input :disabled="isEdit || isView "  v-model="studentForm.sno"  suffix-icon="EditPen" ></el-input>
                 </el-form-item>
@@ -124,7 +136,8 @@
                   </el-select>
                 </el-form-item>
                 <el-form-item label="生日:" prop="birthday">
-                  <el-date-picker :disabled="isView"  v-model="studentForm.birthday" type="date" placeholder="请选择日期" style="width: 100%;">
+                  <el-date-picker format="YYYY/MM/DD"
+                                  value-format="YYYY-MM-DD" :disabled="isView"  v-model="studentForm.birthday" type="date" placeholder="请选择日期" style="width: 100%;">
                   </el-date-picker>
                 </el-form-item>
                 <el-form-item label="手机号码:" prop="mobile">
@@ -139,7 +152,7 @@
               </el-form>
               <span slot="footer" class="dialog-footer" style="margin-left: 33%;">
                 <el-button @click="closeDialogForm()"  type="info" size="small">取消</el-button>
-                <el-button v-show="!isView" type="primary" size="small">确定</el-button>
+                <el-button @click="submitForm()" v-show="!isView" type="primary" size="small">确定</el-button>
               </span>
             </el-dialog>
 
@@ -158,7 +171,7 @@
 import {ref, reactive, toRefs, onMounted} from 'vue';
 import axios from 'axios';
 import {Delete, Edit, Message, MoreFilled} from "@element-plus/icons-vue";
-import {ElMessage} from "element-plus";
+import {ElMessage, ElMessageBox} from "element-plus";
 const input = ref('')
 const pageSizeList = ref([1]);
 const isCollapse = ref(true);
@@ -173,6 +186,84 @@ const handleOpen = (key, keyPath) => {
 const handleClose = (key, keyPath) => {
   console.log(key, keyPath)
 }
+const multipleTableRef = ref()
+const multipleSelection = ref([])
+const handleSelectionChange = (val) => {
+  //console.log(val.map(obj=>parseInt(obj.sno)))
+  multipleSelection.value = val.map(obj=>parseInt(obj.sno))
+
+  console.log(multipleSelection.value)
+  //multipleSelection
+}
+const studentDeletes =()=>{
+  //console.log(multipleSelection.value)
+  const snos = multipleSelection.value
+  console.log(snos.length)
+  if(snos.length == 0){
+    ElMessage({
+      message: '您还没有选择删除的内容！',
+      type: 'info',
+    })
+  }
+  if(snos.length > 0){
+    ElMessageBox.confirm(
+        '您确认删除该数据?',
+        'Warning',
+        {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'warning',
+        }
+    )
+        .then(() => {
+          const snosObj = {
+            'snos':snos
+          }
+          const data = JSON.stringify(snosObj)
+          console.log(data,snosObj)
+          const config = {
+            headers :{
+              'Content-Type':'application/json',
+              'Authorization': 'Basic dXNlcjAxKzp5amw1NTgwMzA='
+            }
+          };
+
+
+          axios
+              //.patch('/api/student/viewsets/'+parseInt(row.sno)+'/',data,config)
+              .delete(`/api/student/viewsets/bulk_destroy/`, {headers:config.headers,data:data})
+              .then(res=>{
+                console.log(res)
+                if(res.status === 204){
+                  ElMessage({
+                    message: '数据删除成功！',
+                    type: 'success',
+                  })
+                }
+              })
+              .catch(err=>{
+                ElMessage({
+                  message: '数据删除失败！',err,
+                  type: 'error',
+                })
+
+              })
+
+
+
+        })
+        .catch(() => {
+          ElMessage({
+            type: 'info',
+            message: '取消删除',
+          })
+        })
+
+  }
+
+}
+
+
 const searchStudent = async (query)=>{
   console.log(query)
   try {
@@ -205,6 +296,8 @@ const searchStudent = async (query)=>{
 }
 //关闭弹出框的表单
 const closeDialogForm=()=>{
+
+  studentFormRef.value.resetFields();
   studentForm.value = '';
   dialogVisible.value=false;
   isEdit.value=false;
@@ -215,12 +308,67 @@ const closeDialogForm=()=>{
 
 
 const viewStudent = async(row)=>{
+  //console.log(row)
   dialogTitle.value='查看数据'
-  isView.value = true
+  isView.value = true;
   dialogVisible.value=true;
   studentForm.value=row;
 }
+
+const deleteStudent = async (row)=>{
+  ElMessageBox.confirm(
+      '您确认删除该数据?',
+      'Warning',
+      {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'warning',
+      }
+  )
+      .then(() => {
+
+        const sno = parseInt(row.sno)
+
+        console.log('delete ',sno)
+        const config = {
+          headers :{
+            'Content-Type':'application/json',
+            'Authorization': 'Basic dXNlcjAxKzp5amw1NTgwMzA='
+
+          }
+        };
+
+        axios
+            //.patch('/api/student/viewsets/'+parseInt(row.sno)+'/',data,config)
+            .delete('/api/student/viewsets/'+sno+'/',config)
+            .then(res=>{
+              console.log(res)
+              if(res.status === 204){
+                ElMessage({
+                  message: '数据删除成功！',
+                  type: 'success',
+                })
+              }
+            })
+            .catch(err=>{
+              ElMessage({
+                message: '数据删除失败！',err,
+                type: 'error',
+              })
+
+            })
+      })
+      .catch(() => {
+        ElMessage({
+          type: 'info',
+          message: '取消删除',
+        })
+      })
+}
+
+
 const editStudent = async(row)=>{
+  //console.log(row)
   dialogTitle.value='编辑数据'
   isEdit.value=true;
   dialogVisible.value=true;
@@ -228,6 +376,8 @@ const editStudent = async(row)=>{
 }
 
 const addStudent = async ()=>{
+  isEdit.value=false;
+  isView.value = false;
   dialogTitle.value='添加数据'
   dialogVisible.value=true;
 }
@@ -238,8 +388,8 @@ const small = ref(false)
 const background = ref(false)
 const disabled = ref(false)
 const totalCount = ref(1)
-const studentFormRef = ref(null)
-const studentForm = reactive({
+const studentFormRef = ref()
+const studentForm = ref({
   sno:'',
   name:'',
   gender:'',
@@ -247,19 +397,26 @@ const studentForm = reactive({
   birthday:'',
   email:'',
   address:'',
-  image:''
+  image:null
 })
 const validSno = async(rule,value,callback)=>{
+  if(dialogTitle.value ==='编辑数据'){
+    callback()
+  }
   try {
     const config = {
       headers :{
-        'Authorization': 'Basic dXNlcjAxKzp5amw1NTgwMzA='
-
+        'Authorization': 'Basic dXNlcjAxKzp5amw1NTgwMzA=',
+        'Content-Tpye':'application/json'
       }
     };
+
     //console.log('/api/student/viewsets/?'+query._value)
-    const response = await axios.post('/api/student/viewsets/',{sno:value},config);
-    console.log(response.data)
+    const response = await axios.post('/api/student/viewsets/',
+        {sno:value}
+
+    ,config);
+    //console.log(response.data)
     //console.log(response)
     ElMessage({
       message: '数据加载成功！',
@@ -268,13 +425,14 @@ const validSno = async(rule,value,callback)=>{
     //this.tableData = response.data.results;
     data1.value = response.data.results;
     totalCount.value = response.data.count
-
+    callback()
 
   } catch (error) {
     // ElMessage({
     //   message: JSON.parse(error.request.response).sno[0],
     //   type: 'error',
     // })
+    //学号唯一性验证
     if('sno' in JSON.parse(error.request.response)){
       callback(new Error(JSON.parse(error.request.response).sno[0]));
     } else{
@@ -285,10 +443,10 @@ const validSno = async(rule,value,callback)=>{
   }
 
 }
-const rules=reactive({
+const rules=ref({
   sno:[
     {validator:validSno,trigger: 'blur'},
-    {pattern:/^[9][5]\d{3}$/,message:'学号必须是95开头',trigger: blur},
+    {pattern:/^[9][5]\d{3}$/,message:'学号必须是95开头的五位数字',trigger: blur},
     {required:true,message:'学号不能为空。',trigger:'blur'},
 
   ],
@@ -371,7 +529,7 @@ const fetchData = async (queryPage,queryPageSize) => {
       message: error,
       type: 'error',
     })
-    console.error(error);
+    //console.error(error);
   }
 };
 
@@ -380,6 +538,112 @@ onMounted(()=>{
 
 }
 )
+
+
+// const submitForm = async ()=>{
+//   await studentFormRef.value?.validate().catch(err=>{
+//     ElMessage.error('表单效验失败')
+//     throw err
+//   })
+//   console.log(studentForm)
+// }
+const submitForm = ()=>{
+
+  studentFormRef.value.validate((valid,fields)=>{
+
+    if(valid){
+
+      if(dialogTitle.value ==='添加数据'){
+
+        //添加
+        submitAddStudent()
+
+      }
+      if(dialogTitle.value ==='编辑数据'){
+        //修改
+        submitEditAddStudent()
+
+      }
+    }else{
+      console.log('error')
+      return false
+    }
+  })
+}
+//添加数据
+const submitAddStudent= ()=>{
+  const row = studentForm.value
+  const data = JSON.stringify(row)
+  console.log('end submit',data)
+  const config = {
+    headers :{
+      'Content-Type':'application/json',
+      'Authorization': 'Basic dXNlcjAxKzp5amw1NTgwMzA='
+
+    }
+  };
+
+  axios
+      .post('/api/student/viewsets/',data,config)
+      .then(res=>{
+        if(res.status === 201){
+          ElMessage({
+            message: '数据添加成功！',
+            type: 'success',
+          })
+        }
+      })
+      .catch(err=>{
+        console.log('添加失败',err)
+      })
+
+
+}
+
+//修改数据
+const submitEditAddStudent= ()=>{
+  const row1 = studentForm.value
+  //sno=parseInt(row.sno)
+  const sno = parseInt(row1.sno)
+
+  const data = JSON.stringify(row1)
+  console.log('end submit',data)
+  const config = {
+    headers :{
+      'Content-Type':'application/json',
+      'Authorization': 'Basic dXNlcjAxKzp5amw1NTgwMzA='
+
+    }
+  };
+
+  axios
+      //.patch('/api/student/viewsets/'+parseInt(row.sno)+'/',data,config)
+      .patch('/api/student/viewsets/'+sno+'/',data,config)
+      .then(res=>{
+        console.log(res)
+        if(res.status === 200){
+          ElMessage({
+            message: '数据修改成功！',
+            type: 'success',
+          })
+        }
+      })
+      .catch(err=>{
+
+        console.log('数据修改失败',err)
+      })
+}
+
+const imageUrl = ref('')
+
+const handleSuccess = (response,file,fileList) => {
+  console.log('图片上传成功',response)
+}
+
+const handleError=(err, file, fileList)=> {
+  console.error('图片上传失败', err);
+}
+
 
 
 
@@ -426,5 +690,29 @@ height:1000px;overflow: hidden;
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
+}
+.el-dialog .avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.el-dialog .avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+.el-dialog .avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+.el-dialog .el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
 }
 </style>
