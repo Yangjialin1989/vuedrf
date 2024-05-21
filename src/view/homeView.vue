b9ae31add2a36f3f6d430085dac0c79739a553e1
Yangjialin <729850713@qq.com>
2024-05-21 09:47:54 +0800

    drf-v3

--- src/view/homeView.vue
+++ src/view/homeView.vue
@@ -78,10 +78,10 @@
               <el-table-column prop="email" label="邮箱" width="180" />
               <el-table-column prop="address" label="地址" width="200" />
               <el-table-column prop="image" label="头像" width="60" />
-              <el-table-column label="操作" width="140" fixed="right">
-                <el-button @click="viewStudent()" type="success" :icon="MoreFilled" circle size="small"/>
-                <el-button type="primary" :icon="Message" circle size="small"/>
-                <el-button type="danger" :icon="Delete" circle size="small"/>
+              <el-table-column label="操作" width="140" fixed="right" v-slot="scope">
+                  <el-button @click="viewStudent(scope.row)" type="success" :icon="MoreFilled" circle size="small"/>
+                  <el-button @click="editStudent(scope.row)" type="primary" :icon="Message" circle size="small"/>
+                  <el-button type="danger" :icon="Delete" circle size="small"/>
               </el-table-column>
             </el-table>
 
@@ -92,11 +92,11 @@
               </el-col>
               <el-col :span="16">
                 <div class="demo-pagination-block">
-
+                  <!--                      :page-sizes-->
                   <el-pagination
                       v-model:current-page="currentPage4"
                       v-model:page-size="pageSize4"
-                      :page-sizes
+
                       :small="small"
                       :disabled="disabled"
                       :background="background"
@@ -109,37 +109,37 @@
               </el-col>
             </el-row>
 
-            <el-dialog title="学生明细" v-model="dialogVisible" width="50%" :close-on-click-modal="false">
-              <el-form :inline="true" size="small" label-width="110px" label-position="right" style="margin-left: 20px;">
-                <el-form-item label="学号:">
-                  <el-input suffix-icon="EditPen" ></el-input>
+            <el-dialog :title="dialogTitle" v-model="dialogVisible" width="50%" :close-on-click-modal="false" @close="closeDialogForm()">
+              <el-form ref="studentFormRef" :model="studentForm" :rules="rules" :inline="true" size="small" label-width="110px" label-position="right" style="margin-left: 20px;">
+                <el-form-item label="学号:" prop="sno">
+                  <el-input :disabled="isEdit || isView "  v-model="studentForm.sno"  suffix-icon="EditPen" ></el-input>
                 </el-form-item>
-                <el-form-item label="姓名:">
-                  <el-input suffix-icon="EditPen" ></el-input>
+                <el-form-item label="姓名:" prop="name">
+                  <el-input :disabled="isView" v-model="studentForm.name" suffix-icon="EditPen" ></el-input>
                 </el-form-item>
-                <el-form-item label="性别:">
-                  <el-select placeholder="Select" style="width:188px;"  size="small">
+                <el-form-item label="性别:" prop="gender">
+                  <el-select :disabled="isView" v-model="studentForm.gender" placeholder="Select" style="width:188px;"  size="small">
                     <el-option label="男" value="男"></el-option>
                     <el-option label="女" value="女"></el-option>
                   </el-select>
                 </el-form-item>
-                <el-form-item label="生日:">
-                  <el-date-picker type="date" placeholder="请选择日期" style="width: 100%;">
+                <el-form-item label="生日:" prop="birthday">
+                  <el-date-picker :disabled="isView"  v-model="studentForm.birthday" type="date" placeholder="请选择日期" style="width: 100%;">
                   </el-date-picker>
                 </el-form-item>
-                <el-form-item label="手机号码:">
-                  <el-input suffix-icon="EditPen" ></el-input>
+                <el-form-item label="手机号码:" prop="mobile">
+                  <el-input  :disabled="isView" v-model="studentForm.mobile" suffix-icon="EditPen" ></el-input>
                 </el-form-item>
-                <el-form-item label="邮箱:">
-                  <el-input suffix-icon="EditPen" ></el-input>
+                <el-form-item label="邮箱:" prop="email">
+                  <el-input  :disabled="isView"  v-model="studentForm.email" suffix-icon="EditPen" ></el-input>
                 </el-form-item>
-                <el-form-item label="家庭住址:">
-                  <el-input suffix-icon="EditPen" ></el-input>
+                <el-form-item label="家庭住址:" prop="address">
+                  <el-input  :disabled="isView" v-model="studentForm.address" suffix-icon="EditPen" ></el-input>
                 </el-form-item>
               </el-form>
               <span slot="footer" class="dialog-footer" style="margin-left: 33%;">
-                <el-button  type="info" size="small">取消</el-button>
-                <el-button type="primary" size="small">确定</el-button>
+                <el-button @click="closeDialogForm()"  type="info" size="small">取消</el-button>
+                <el-button v-show="!isView" type="primary" size="small">确定</el-button>
               </span>
             </el-dialog>
 
@@ -160,11 +160,13 @@ import axios from 'axios';
 import {Delete, Edit, Message, MoreFilled} from "@element-plus/icons-vue";
 import {ElMessage} from "element-plus";
 const input = ref('')
-const pageSizeList = ref([1])
-const isCollapse = ref(true)
-const inputSearch = ref('查询内容')
-const dialogVisible = ref(false)
-
+const pageSizeList = ref([1]);
+const isCollapse = ref(true);
+const inputSearch = ref('查询内容');
+const dialogVisible = ref(false);
+const dialogTitle = ref('');
+const isView = ref(false);
+const isEdit = ref(false);
 const handleOpen = (key, keyPath) => {
   console.log(key, keyPath)
 }
@@ -201,12 +203,32 @@ const searchStudent = async (query)=>{
     console.error(error);
   }
 }
-const viewStudent = async()=>{
-  dialogVisible.value=true;
+//关闭弹出框的表单
+const closeDialogForm=()=>{
+  studentForm.value = '';
+  dialogVisible.value=false;
+  isEdit.value=false;
+  isView.value=false;
 
 }
 
+
+
+const viewStudent = async(row)=>{
+  dialogTitle.value='查看数据'
+  isView.value = true
+  dialogVisible.value=true;
+  studentForm.value=row;
+}
+const editStudent = async(row)=>{
+  dialogTitle.value='编辑数据'
+  isEdit.value=true;
+  dialogVisible.value=true;
+  studentForm.value=row;
+}
+
 const addStudent = async ()=>{
+  dialogTitle.value='添加数据'
   dialogVisible.value=true;
 }
 const data1 = ref([])
@@ -216,6 +238,87 @@ const small = ref(false)
 const background = ref(false)
 const disabled = ref(false)
 const totalCount = ref(1)
+const studentFormRef = ref(null)
+const studentForm = reactive({
+  sno:'',
+  name:'',
+  gender:'',
+  mobile:'',
+  birthday:'',
+  email:'',
+  address:'',
+  image:''
+})
+const validSno = async(rule,value,callback)=>{
+  try {
+    const config = {
+      headers :{
+        'Authorization': 'Basic dXNlcjAxKzp5amw1NTgwMzA='
+
+      }
+    };
+    //console.log('/api/student/viewsets/?'+query._value)
+    const response = await axios.post('/api/student/viewsets/',{sno:value},config);
+    console.log(response.data)
+    //console.log(response)
+    ElMessage({
+      message: '数据加载成功！',
+      type: 'success',
+    })
+    //this.tableData = response.data.results;
+    data1.value = response.data.results;
+    totalCount.value = response.data.count
+
+
+  } catch (error) {
+    // ElMessage({
+    //   message: JSON.parse(error.request.response).sno[0],
+    //   type: 'error',
+    // })
+    if('sno' in JSON.parse(error.request.response)){
+      callback(new Error(JSON.parse(error.request.response).sno[0]));
+    } else{
+      callback()
+
+    }
+
+  }
+
+}
+const rules=reactive({
+  sno:[
+    {validator:validSno,trigger: 'blur'},
+    {pattern:/^[9][5]\d{3}$/,message:'学号必须是95开头',trigger: blur},
+    {required:true,message:'学号不能为空。',trigger:'blur'},
+
+  ],
+  name:[
+    {required:true,message:'姓名不能为空。',trigger:'blur'},
+    {pattern:/^[\u4e00-\u9fa5]{2,5}$/,message: '姓名必须是2-5个汉字',trigger: 'blur'}
+  ],
+  gender:[
+    {required:true,message:'性别不能为空。',trigger:'change'}
+  ],
+  birthday:[
+    {type:'date',required:true,message:'生日不能为空。',trigger:'change'},
+
+  ],
+  mobile:[
+    {required:true,message:'手机号不能为空。',trigger:'blur'},
+    {pattern:/^[1][356789]\d{9}$/,message: '请输入正确的手机号',trigger: 'blur'}
+  ],
+  email:[
+    {required:true,message:'邮箱不能为空。',trigger:'blur'},
+    {pattern:/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,message: '请输入正确的邮箱',trigger: 'blur'}
+  ],
+  address:[
+    {required:true,message:'住址不能为空。',trigger:'blur'},
+  ]
+})
+
+
+
+
 const handleSizeChange = (val) => {
   console.log(`page_size=${val} `)
   queryPage.value = 'page=1'
